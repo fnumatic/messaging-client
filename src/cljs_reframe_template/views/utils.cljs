e67d3922e9eaa51cf0ba18ac2338e98ca51eca5c
Ulf Ninow <mail@fnumatic.de>
2021-02-16 16:37:04 +0100

    feat: opt in spread func

--- src/cljs_reframe_template/views/utils.cljs
+++ src/cljs_reframe_template/views/utils.cljs
@@ -1,10 +1,13 @@
 (ns cljs-reframe-template.views.utils)
 
 
-(defn spread-by-id [comp coll ]
-  (for [item coll]
-    ^{:key (:id item)} [comp item]))
+(defn spread-by-id [comp coll & opts ]
+  (let [enrich-item #(if opts (merge % (first opts)) %)]
+   (for [item coll]
+     ^{:key (:id item)}
+     [comp (enrich-item item)])))
 
-(defn spread-by-order [comp coll ]
-  (for [[idx item] (map-indexed vector coll)]
-    ^{:key idx} [comp item]))
+(defn spread-by-order [comp coll & opts]
+  (let [enrich-item #(if opts (merge % (first opts)) %)]
+   (for [[idx item] (map-indexed vector coll)]
+    ^{:key idx} [comp (enrich-item item)])))
